eea40530831189c010a492216cf855dc0a0ac929
PB Sathyanarayan <pleasantnow@gmail.com>
2023-05-30 08:50:23 +0530

    files added in spring-core

---
+++ 02-spring-boot-spring-core/04-qualifiers/springcoredemo/src/main/java/com/luv2code/springcoredemo/common/TennisCoach.java
@@ -0,0 +1,13 @@
+package com.luv2code.springcoredemo.common;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class TennisCoach implements  Coach{
+
+
+    @Override
+    public String getDailyWorkout() {
+        return "practice your backhand volley";
+    }
+}
